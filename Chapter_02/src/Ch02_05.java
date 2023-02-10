438a951a72387ebc91516c2863a37afbc9aa050a
rlahjxx <ddh4473@naver.com>
2023-02-10 20:16:20 +0900

    change dir and day 3

---
+++ Chapter_02/src/Ch02_05.java
@@ -0,0 +1,9 @@
+public class Ch02_05 {
+    public static void main(String args[]) {
+        final int MAX_SPEED = 10;
+        int year = 2014;
+
+        System.out.println(MAX_SPEED);
+        System.out.println(year);
+    }
+}
