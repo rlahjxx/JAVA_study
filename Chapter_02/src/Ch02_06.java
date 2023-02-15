7daab4bba7791c81445f1da8b1425cac8351fa75
rlahjxx <ddh4473@naver.com>
2023-02-15 20:50:35 +0900

    리미털의 타입과 접미사

---
+++ Chapter_02/src/Ch02_06.java
@@ -0,0 +1,9 @@
+public class Ch02_06 {
+    public static void main(String args[]) {
+        int octNum = 010 ; // 8 진수 10 , 10 진수로 8
+        int hexNum = 0x10 ; // 16 진수 10 , 10 진수로 16
+
+        System.out.println(octNum);
+        System.out.println(hexNum);
+    }
+}
