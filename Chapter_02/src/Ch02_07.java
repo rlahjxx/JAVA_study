e605c1207fe8057c9e0c980e329ab5d3dfbf8ea9
rlahjxx <ddh4473@naver.com>
2023-02-17 11:54:54 +0900

    문자 리터럴과 문자열 리터럴

---
+++ Chapter_02/src/Ch02_07.java
@@ -0,0 +1,11 @@
+public class Ch02_07 {
+    public static void main(String args[]) {
+        char ch = 'J';
+        String name = "Java";
+
+        String name2 = new String("Java");
+
+        System.out.println(ch + name + name2);
+        
+    }
+}
