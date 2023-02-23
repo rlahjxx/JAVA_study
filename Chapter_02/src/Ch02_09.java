5555bffea984eb9ae66317c00406bc4b2c4f45f6
rlahjxx <ddh4473@naver.com>
2023-02-23 18:50:11 +0900

    두 변수의 값 바꾸기

---
+++ Chapter_02/src/Ch02_09.java
@@ -0,0 +1,15 @@
+public class Ch02_09 {
+    public static void main(String args[]) {
+        int x = 10 , y = 5;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        int tmp = x;
+        x = y;
+        y = tmp;
+        
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
