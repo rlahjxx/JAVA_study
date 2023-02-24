43079d300cff56f64bfb7660b177065d8c428cec
rlahjxx <ddh4473@naver.com>
2023-02-24 17:16:35 +0900

    기본형과 참조형

---
+++ Chapter_02/src/Ch02_10.java
@@ -0,0 +1,16 @@
+public class Ch02_10 {
+    public static void main(String args[]) {
+        // 문자형
+        char a ;
+
+        // 정수형
+        byte b ;
+        short c ;
+        int d ;
+        long e ;
+
+        // 실수형
+        float f ;
+        double g;
+    }
+}
