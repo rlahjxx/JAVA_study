be4836726b96144ee269d338d35228ac65653fab
rlahjxx <ddh4473@naver.com>
2023-03-05 20:01:08 +0900

    연산자와 피연산자

---
+++ Chapter_03/src/Ch03_01.java
@@ -0,0 +1,9 @@
+public class Ch03_01 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 4 * x + 3;
+        
+        System.out.println(y);
+        System.out.println(4 * 5 + 3);
+    }
+}
