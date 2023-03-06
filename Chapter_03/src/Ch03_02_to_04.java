378e31d4b3f0c0d5fdaa2fc515de3c8dded69616
rlahjxx <ddh4473@naver.com>
2023-03-06 21:16:38 +0900

    연산자의 결합규칙

---
+++ Chapter_03/src/Ch03_02_to_04.java
@@ -0,0 +1,10 @@
+public class Ch03_02_to_04 {
+    public static void main(String[] args) {
+        int x, y;
+
+        x = y = 3; // y 에 3 이 저장된 후, x 에 3 이 저장된다.
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
