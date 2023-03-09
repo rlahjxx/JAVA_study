92f1a9f5a580cc40a5f66e557b35bdff4e4d4da7
rlahjxx <ddh4473@naver.com>
2023-03-09 22:22:50 +0900

    증감 연산자 ++ 과 --

--- Chapter_03/src/Ch03_05.java
+++ Chapter_03/src/Ch03_05.java
@@ -10,5 +10,11 @@ public class Ch03_05 {
 
         j = ++i ;
         System.out.println("j = ++i; 실행 후 , i = " + i + " j = " + j);
+
+        i = 5; j = 5;
+        System.out.println(i++);
+        System.out.println(++j);
+
+        System.out.println("i = " + i + ", j =  " + j);
     }
 }
