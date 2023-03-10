4033adf210265e46b8eb5a44fe2adf58cfb68d96
rlahjxx <ddh4473@naver.com>
2023-03-10 20:24:34 +0900

    부호 연산자

---
+++ Chapter_03/src/Ch03_06.java
@@ -0,0 +1,11 @@
+public class Ch03_06 {
+    public static void main(String[] args) {
+        int i = -10;
+        i = +i;
+        System.out.println(i);
+
+        i = -10;
+        i = -i;
+        System.out.println(i);
+    }
+}
