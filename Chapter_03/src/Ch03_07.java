17449ccdc5b1da746de0f5e67f9b583b8948e9c9
rlahjxx <ddh4473@naver.com>
2023-03-13 20:32:25 +0900

    형변환 연산자

--- Chapter_03/src/Ch03_07.java
+++ Chapter_03/src/Ch03_07.java
@@ -1,3 +1,11 @@
+import java.io.ObjectInputFilter.Status;
+
 public class Ch03_07 {
-    
+    public static void main(String[] args) {
+        double d = 85.4;
+        int score = (int) d;
+
+        System.out.println("score = " + score);
+        System.out.println("d = " + d);
+    }
 }
