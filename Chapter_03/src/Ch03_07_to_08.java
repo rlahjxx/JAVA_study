7896ccba48d768f6151d49387f439ace06c559bc
rlahjxx <ddh4473@naver.com>
2023-03-14 19:36:31 +0900

    형변환

--- Chapter_03/src/Ch03_07_to_08.java
+++ Chapter_03/src/Ch03_07_to_08.java
@@ -1,6 +1,6 @@
 import java.io.ObjectInputFilter.Status;
 
-public class Ch03_07 {
+public class Ch03_07_to_08 {
     public static void main(String[] args) {
         double d = 85.4;
         int score = (int) d;
