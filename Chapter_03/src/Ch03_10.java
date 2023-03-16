6ebb845248319535d608acb1d16f533c64754f3a
rlahjxx <ddh4473@naver.com>
2023-03-17 00:07:14 +0900

    산술변환

--- Chapter_03/src/Ch03_10.java
+++ Chapter_03/src/Ch03_10.java
@@ -2,5 +2,10 @@ public class Ch03_10 {
     public static void main(String args[]) {
         System.out.println(5/2);
         System.out.println(5/(float)2);
+
+        byte a = 10;
+        byte b = 30;
+        byte c = (byte)(a * b);
+        System.out.println(c);
     }
 }
